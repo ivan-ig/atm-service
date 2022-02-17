5c5a0ab8f8f69277f3d8d1224fd8f6ac73302eae
ivan-ig <ivan_10_93@bk.ru>
2022-02-18 00:51:46 +0300

    Добавлен слой Service

---
+++ BankServer/src/main/java/com/github/ivanig/bankserver/service/AccountService.java
@@ -0,0 +1,15 @@
+package com.github.ivanig.bankserver.service;
+
+import com.github.ivanig.bankserver.domain.Account;
+import com.github.ivanig.bankserver.dto.RequestFromAtm;
+import com.github.ivanig.bankserver.dto.ResponseToAtm;
+
+import java.util.Set;
+
+public interface AccountService {
+
+    ResponseToAtm prepareResponse(RequestFromAtm requestFromAtm);
+
+    Set<Account> getCardAccounts(RequestFromAtm requestFromAtm);
+
+}
---
+++ BankServer/src/main/java/com/github/ivanig/bankserver/service/AccountServiceImpl.java
@@ -0,0 +1,34 @@
+package com.github.ivanig.bankserver.service;
+
+import com.github.ivanig.bankserver.domain.Account;
+import com.github.ivanig.bankserver.domain.BankClient;
+import com.github.ivanig.bankserver.dto.RequestFromAtm;
+import com.github.ivanig.bankserver.dto.ResponseToAtm;
+import com.github.ivanig.bankserver.repository.AccountRepository;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.util.Set;
+
+@Data
+public class AccountServiceImpl implements AccountService {
+
+    @NonNull
+    private AccountRepository accountRepository;
+
+    @Override
+    public ResponseToAtm prepareResponse(RequestFromAtm request) {
+        return new ResponseToAtm(getCardAccounts(request));         // ???
+    }
+
+    @Override
+    public Set<Account> getCardAccounts(RequestFromAtm request) {
+        String firstName = request.getFirstName();
+        String lastName = request.getLastName();
+        long cardNumber = request.getCardNumber();
+
+        BankClient client = accountRepository.getClientFromDataBase(firstName, lastName, cardNumber);
+
+        return client.getCardAccountsOnly();
+    }
+}
