a8975f3152343a9079f7d8e3a3b7b67218b58174
ivan-ig <ivan_10_93@bk.ru>
2022-03-23 23:50:22 +0300

    Обновлены тесты

--- AtmServer/src/test/java/com/github/ivanig/atmserver/service/AtmServiceTest.java
+++ AtmServer/src/test/java/com/github/ivanig/atmserver/service/AtmServiceTest.java
@@ -1,34 +1,84 @@
 package com.github.ivanig.atmserver.service;
 
-import com.github.ivanig.atmserver.dto.ResponseToClient;
+import com.github.ivanig.common.messages.RequestFromAtm;
 import com.github.ivanig.common.messages.ResponseToAtm;
+import lombok.SneakyThrows;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 class AtmServiceTest {
 
-//    static AtmService atmService;
-//
-//    @BeforeAll
-//    static void setUp() {
-//        atmService = new AtmService();
-//    }
-//
-//    @Test
-//    public void checkResponsePreparing() {
-//
-//    Map<String, String> accountInfo = new HashMap<>();
-//    accountInfo.put("77777777777777711111", "0.00 RUB");
-//    ResponseToAtm responseFromBank = new ResponseToAtm(accountInfo);
-//
-//    ResponseToClient expectedResponse = new ResponseToClient(accountInfo);
-//
-//    assertEquals(expectedResponse, atmService.getResponseToClient(responseFromBank));
-//
-//    }
+    private static MockWebServer mws;
+    private AtmService atmService;
+
+    @BeforeAll
+    static void setUp() throws IOException {
+        mws = new MockWebServer();
+        mws.start();
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        mws.shutdown();
+    }
+
+    @BeforeEach
+    void init() {
+        atmService = new AtmService("http://localhost:" + mws.getPort());
+    }
+
+    @SneakyThrows
+    @Test
+    public void successGetClientInfoFromBank() {
+        RequestFromAtm request = new RequestFromAtm("fn", "ln", 16L, 1111);
+
+        mws.enqueue(new MockResponse()
+                .setBody("{\"firstname\":\"fn\",\"patronymic\":\"pa\"," +
+                        "\"accountsAndBalances\":{\"20\":\"0.00 EUR\"," +
+                                                 "\"21\":\"0.00 USD\"," +
+                                                 "\"22\":\"0.00 RUB\"}," +
+                        "\"pinCodeStatus\":\"OK\"}")
+                .addHeader("Content-Type", "application/json"));
+
+        Mono<ResponseToAtm> response = atmService.getClientInfoFromBank(request);
+
+        StepVerifier
+                .create(response)
+                .expectNextMatches(r ->
+                        request.getFirstName().equals(r.getFirstname()))
+                .verifyComplete();
+
+        RecordedRequest recordedRequest = mws.takeRequest();
+
+        assertEquals("POST", recordedRequest.getMethod());
+        assertEquals("/clientInfo", recordedRequest.getPath());
+    }
+
+    @Test
+    public void failGetClientInfoFromBank() {
+
+    }
+
+    @Test
+    public void successAnalyzeAndConvertToResponseForClient() {
+
+    }
+
+    @Test
+    public void failAnalyzeAndConvertToResponseForClient() {
+
+    }
 }
--- BankServer/src/test/java/com/github/ivanig/bankserver/service/BankServiceTest.java
+++ BankServer/src/test/java/com/github/ivanig/bankserver/service/BankServiceTest.java
@@ -1,46 +1,133 @@
 package com.github.ivanig.bankserver.service;
 
+import com.github.ivanig.bankserver.entities.Account;
+import com.github.ivanig.bankserver.entities.Client;
+import com.github.ivanig.bankserver.repository.ClientRepository;
+import com.github.ivanig.common.messages.PinCodeStatus;
+import com.github.ivanig.common.messages.RequestFromAtm;
+import com.github.ivanig.common.messages.ResponseToAtm;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class BankServiceTest {
 
-//    private static BankRepository bankRepository;
-//    private static BankService bankService;
-//    private static RequestFromAtm request;
-//
-//    @BeforeAll
-//    public static void setUp() {
-//
-//        bankRepository = Mockito.mock(BankRepository.class);
-//        bankService = new BankService(bankRepository);
-//
-//        request = new RequestFromAtm("1", "2", 16L, 1234);
-//    }
-//
-//    @Test
-//    public void successPrepareResponse() {
-//
-//        Set<Account> accounts = new HashSet<Account>(){{
-//            add(new Account("20", null, "RUB", new BigDecimal("0.00")));
-//            add(new Account("21", 16L, "RUB", new BigDecimal("0.00")));
-//            add(new Account("22", 16L, "USD", new BigDecimal("0.00")));
-//        }};
-//
-//        when(bankRepository.getClient(request)).then(e ->
-//                Optional.of(new BankClient("any", "any", accounts)));
-//
-//        Map<String, String> map = new HashMap<String, String>() {{
-//            put("21", "0.00 RUB");
-//            put("22", "0.00 USD");
-//        }};
-//        ResponseToAtm expectedResponse = new ResponseToAtm(map);
-//
-//        Assertions.assertEquals(expectedResponse, bankService.getAccountsInfoConvertedToResponse(request));
-//    }
-//
-//    @Test
-//    public void failPrepareResponse() {
-//
-//        when(bankRepository.getClient(request)).then(e -> Optional.ofNullable(null));
-//
-//        Assertions.assertThrows(ClientNotFoundException.class, () -> bankService.getAccountsInfoConvertedToResponse(request));
-//    }
+    @Autowired
+    private MockMvc mvc;
+
+    private static ClientRepository clientRepository;
+    private static BankService bankService;
+    private static Set<Account> accounts;
+    private static Set<Client> clients;
+
+    @BeforeAll
+    public static void setUp() {
+        clientRepository = Mockito.mock(ClientRepository.class);
+        bankService = new BankService(clientRepository);
+
+        accounts = new HashSet<Account>() {{
+            Account account = new Account();
+            account.setId(1L);
+            account.setNumber("20");
+            account.setCardNumber(16L);
+            account.setPinCode(1234);
+            account.setAmount(new BigDecimal("0.00"));
+            account.setCurrency("RUB");
+            add(account);
+        }};
+
+        clients = new HashSet<Client>() {{
+            Client client = new Client();
+            client.setId(1L);
+            client.setFirstName("fn");
+            client.setPatronymic("pa");
+            client.setLastName("ln");
+            client.setAccounts(accounts);
+            add(client);
+        }};
+    }
+
+    @Test
+    public void successGetAccountsAndConvertToResponse() {
+        RequestFromAtm request = new RequestFromAtm("fn", "ln", 16L, 1234);
+
+        when(clientRepository.findClientsByFirstNameAndLastName(request.getFirstName(), request.getLastName()))
+                .then(e -> clients);
+
+        Map<String,String> accountsAndBalances = new HashMap<String,String>() {{
+            put("20", "0.00 RUB");
+        }};
+        ResponseToAtm expectedResponse = new ResponseToAtm(
+                "fn", "pa", accountsAndBalances, PinCodeStatus.OK);
+
+        Assertions.assertEquals(expectedResponse, bankService.getCardAccountsInfoAndConvertToResponse(request));
+    }
+
+    @Test
+    public void successGetAccountsAndConvertToResponse_InvalidPinCode() {
+        RequestFromAtm request = new RequestFromAtm("fn", "ln", 16L, 9999);
+
+        when(clientRepository.findClientsByFirstNameAndLastName(request.getFirstName(), request.getLastName()))
+                .then(e -> clients);
+
+        ResponseToAtm expectedResponse = new ResponseToAtm(
+                "fn", "pa", new HashMap<>(), PinCodeStatus.INVALID);
+
+        Assertions.assertEquals(expectedResponse, bankService.getCardAccountsInfoAndConvertToResponse(request));
+    }
+
+    @Test
+    public void successGetResponse() throws Exception {
+        mvc.perform(post("/clientInfo").contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"firstName\":\"ANDREY\"," +
+                                "\"lastName\":\"VASILIEV\"," +
+                                "\"cardNumber\":\"1616161616161111\"," +
+                                "\"pinCode\":\"1234\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("{\"firstname\":\"ANDREY\"," +
+                        "\"patronymic\":\"IVANOVICH\"," +
+                        "\"accountsAndBalances\":{\"77777777777777733333\":\"0.00 EUR\"," +
+                        "\"77777777777777722222\":\"0.00 USD\"," +
+                        "\"77777777777777711111\":\"0.00 RUB\"}," +
+                        "\"pinCodeStatus\":\"OK\"}"));
+    }
+
+    @Test
+    public void failGetResponse_4xxCodeDueToInvalidCardNumber() throws Exception {
+        mvc.perform(post("/clientInfo").contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"firstName\":\"AFANASII\"," +
+                                "\"lastName\":\"FET\"," +
+                                "\"cardNumber\":\"9999999\"," +
+                                "\"pinCode\":\"1234\"}"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void failGetResponse_4xxCodeDueToInvalidClientName() throws Exception {
+        mvc.perform(post("/clientInfo").contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"firstName\":\"BOBA\"," +
+                                "\"lastName\":\"FETT\"," +
+                                "\"cardNumber\":\"1616161616162222\"," +
+                                "\"pinCode\":\"1234\"}"))
+                .andExpect(status().is4xxClientError());
+    }
 }
