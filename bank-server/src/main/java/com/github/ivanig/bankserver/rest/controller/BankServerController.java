ba9537d30c9484347f65030bfdc3f7b175664303
ivan-ig <ivan_10_93@bk.ru>
2022-04-28 19:49:51 +0300

    Рефакторинг контроллера bank-server'а

--- bank-server/src/main/java/com/github/ivanig/bankserver/rest/controller/BankServerController.java
+++ bank-server/src/main/java/com/github/ivanig/bankserver/rest/controller/BankServerController.java
@@ -2,15 +2,12 @@ package com.github.ivanig.bankserver.rest.controller;
 
 import com.github.ivanig.common.messages.RequestFromAtm;
 import com.github.ivanig.common.messages.ResponseToAtm;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public interface BankServerController {
 
-    @PostMapping(path = "/clientInfo",
-                 consumes = MediaType.APPLICATION_JSON_VALUE,
-                 produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseToAtm getClientInfo(@RequestBody RequestFromAtm request);
 
 }
--- bank-server/src/main/java/com/github/ivanig/bankserver/rest/controller/BankServerControllerImpl.java
+++ bank-server/src/main/java/com/github/ivanig/bankserver/rest/controller/BankServerControllerImpl.java
@@ -5,7 +5,9 @@ import com.github.ivanig.common.messages.RequestFromAtm;
 import com.github.ivanig.common.messages.ResponseToAtm;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Component
@@ -15,6 +17,9 @@ public class BankServerControllerImpl implements BankServerController {
     private BankServerService bankServerService;
 
     @Override
+    @PostMapping(path = "/clientInfo",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseToAtm getClientInfo(RequestFromAtm request) {
         log.debug("Received(REST): [" + request + "]");
         return bankServerService.getCardAccountsInfoAndConvertToResponse(request);
